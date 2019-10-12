382f3bf2f55610bc0c957faba25f9cfc6c7b3d4b
Alex Lin <h427m4nn.cs05@g2.nctu.edu.tw>
2019-10-12 17:13:48 +0800

    make hw2-1.sh executable

--- HW2/hw2-1.sh
+++ HW2/hw2-1.sh
@@ -1,2 +1,2 @@
-#! bin/sh
+#! /bin/sh
 echo "`ls -lAR | grep ^- | sort -k 5 -n -r | head -n 5 | awk '{print NR ": " $5 " " $9}'``ls -lAR | grep ^d | wc -l | awk '{print "\nDir num: "$1}'``ls -lAR | grep ^- | wc -l | awk '{print "\nFile num: "$1}'``ls -lAR | grep ^- | awk '{total+=$5}; END {print "\nTotal: "total}'`"
